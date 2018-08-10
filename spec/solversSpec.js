4289c88bb848f0757da01acfe11abd4e252fa6a7
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-09 19:30:23 -0700

    improved n queens count diagonal

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -69,9 +69,9 @@ describe('solvers', function() {
   describe('countNQueensSolutions()', function() {
 
     it('finds the number of valid solutions for n of 0-8', function() {
-      _.range(0, 9).map(function(n) {
+      _.range(0, 13).map(function(n) {
         var solutionCount = countNQueensSolutions(n);
-        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
+        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200][n];
 
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
--- src/solvers.js
+++ src/solvers.js
@@ -72,7 +72,7 @@ window.countNRooksSolutions = function(n) {
         loopOverRow(0, Math.floor(n / 2));
         solutionCount *= 2; // Runs on half, double the solution count since it's symmetrical
         if (n % 2 !== 0) {
-          loopOverRow(Math.floor((n + 1) / 2), Math.floor((n + 1) / 2) + 1);
+          loopOverRow(Math.floor((n + 1) / 2 - 1), Math.floor((n + 1) / 2));
         }
       }
     } else {
@@ -91,23 +91,35 @@ window.findNQueensSolution = function(n) {
   var solution = undefined;
   var myBoard = new Board({n: n});
   var columnVisited = new Array(n).fill(0);
+  if (n === 0) {
+    var majorVisited = [];
+    var minorVisited = [];
+  } else {
+    var majorVisited = new Array(2 * n - 1).fill(0);
+    var minorVisited = new Array(2 * n - 1).fill(0);
+  }
 
   var testAddQueen = function (currRow) {
     for (var i = 0; i < n; i++) {
       if (solution !== undefined) {
         return;
       }
-      if (columnVisited[i] === 0) {
+      var majorIndex = myBoard._getFirstRowColumnIndexForMajorDiagonalOn(currRow, i) + n - 1;
+      var minorIndex = myBoard._getFirstRowColumnIndexForMinorDiagonalOn(currRow, i);
+      if (columnVisited[i] === 0 && majorVisited[majorIndex] === 0 && minorVisited[minorIndex] === 0) {
         columnVisited[i] = 1;
+        majorVisited[majorIndex] = 1;
+        minorVisited[minorIndex] = 1;
         myBoard.togglePiece(currRow, i);
-        if (!myBoard.hasMajorDiagonalConflictAt(myBoard._getFirstRowColumnIndexForMajorDiagonalOn(currRow, i)) 
-            && !myBoard.hasMinorDiagonalConflictAt(myBoard._getFirstRowColumnIndexForMinorDiagonalOn(currRow, i))) {
-          if (n === currRow + 1 ) {     
-            solution = myBoard.getRows();     
-          }
-          testAddQueen(currRow + 1);   
+        
+        if (n === currRow + 1) {     
+          solution = myBoard.getRows();     
         }
+        testAddQueen(currRow + 1);
+
         columnVisited[i] = 0;
+        majorVisited[majorIndex] = 0;
+        minorVisited[minorIndex] = 0;
         myBoard.togglePiece(currRow, i);
       }
     }
@@ -128,38 +140,46 @@ window.countNQueensSolutions = function(n) {
   var solutionCount = 0;
   if (n === 0) {
     solutionCount = 1;
+    console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+    return solutionCount;
   }
+
   var myBoard = new Board({n: n});
   var columnVisited = new Array(n).fill(0);
+  var majorVisited = new Array(2 * n - 1).fill(0);
+  var minorVisited = new Array(2 * n - 1).fill(0);
 
   var testAddQueen = function(currRow) {
     var loopOverRow = function (start, end) {
       for (var i = start; i < end; i++) {
-        if (columnVisited[i] === 0) {
+        var majorIndex = myBoard._getFirstRowColumnIndexForMajorDiagonalOn(currRow, i) + n - 1;
+        var minorIndex = myBoard._getFirstRowColumnIndexForMinorDiagonalOn(currRow, i);
+        if (columnVisited[i] === 0 && majorVisited[majorIndex] === 0 && minorVisited[minorIndex] === 0) {
           columnVisited[i] = 1;
-          myBoard.togglePiece(currRow, i);
-          if (!myBoard.hasMajorDiagonalConflictAt(myBoard._getFirstRowColumnIndexForMajorDiagonalOn(currRow, i)) 
-            && !myBoard.hasMinorDiagonalConflictAt(myBoard._getFirstRowColumnIndexForMinorDiagonalOn(currRow, i))) {
-            if (n === currRow + 1) {
-              solutionCount++;
-            } else {
-              testAddQueen(currRow + 1);
-            }
+          majorVisited[majorIndex] = 1;
+          minorVisited[minorIndex] = 1;
+
+          if (n === currRow + 1) {
+            solutionCount++;
+          } else {
+            testAddQueen(currRow + 1);
           }
+
           columnVisited[i] = 0;
-          myBoard.togglePiece(currRow, i);
+          majorVisited[majorIndex] = 0;
+          minorVisited[minorIndex] = 0;
         }
       }
     };
 
     if (currRow === 0) {
-      if (n === 0 || n === 1) {
+      if (n === 1) {
         loopOverRow(0, n);
       } else {
         loopOverRow(0, Math.floor(n / 2));
         solutionCount *= 2; // Runs on half, double the solution count since it's symmetrical
         if (n % 2 !== 0) {
-          loopOverRow(Math.floor((n + 1) / 2), Math.floor((n + 1) / 2) + 1);
+          loopOverRow(Math.floor((n + 1) / 2 - 1), Math.floor((n + 1) / 2));
         }
       }
     } else {
